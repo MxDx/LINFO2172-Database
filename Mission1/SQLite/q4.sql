2281027ca9a0ff79c4210bdabfed7181956a61cf
MxDx <maxime.dxl2002@gmail.com>
2024-03-06 10:08:20 +0100

    q4 improvement

--- Mission1/SQLite/q4.sql
+++ Mission1/SQLite/q4.sql
@@ -17,9 +17,11 @@ FROM (
 )
 
 /* Optimization */
-SELECT DISTINCT c1.name AS country1, c2.name AS country2
-FROM CountryCovid cc
-JOIN Borders b ON cc.country = b.country1 AND cc.total_cases = 0 AND cc.year <= 2021 AND cc.month <= 12
-JOIN Country c1 ON b.country1 = c1.code
-JOIN Country c2 ON b.country2 = c2.code
-WHERE SUM(cc.total_cases) = 0;
+SELECT c1.name AS country1, c2.name AS country2
+FROM CountryCovid cv
+LEFT JOIN Country c1 
+ON cv.country = c1.code AND cv.year <= 2021 AND cv.month <= 12
+LEFT JOIN Borders b ON b.country1 = cv.country 
+LEFT JOIN Country c2 ON b.country2 = c2.code
+GROUP BY c1.name, c2.name
+HAVING SUM(cv.total_cases) = 0;
